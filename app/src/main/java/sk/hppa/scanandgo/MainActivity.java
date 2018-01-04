b2a960190082dbaee3831c72ebacd0eff1924569
1havran <havran.init0@gmail.com>
2018-01-04 18:54:44 +0100

    NoFileSpecified but the URI has file://

--- app/src/main/java/sk/hppa/scanandgo/MainActivity.java
+++ app/src/main/java/sk/hppa/scanandgo/MainActivity.java
@@ -1,26 +1,44 @@
 package sk.hppa.scanandgo;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v13.app.ActivityCompat;
 import android.support.v4.content.FileProvider;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.microsoft.onedrivesdk.picker.IPicker;
+import com.microsoft.onedrivesdk.picker.IPickerResult;
+import com.microsoft.onedrivesdk.picker.LinkType;
+import com.microsoft.onedrivesdk.picker.Picker;
+import com.microsoft.onedrivesdk.saver.ISaver;
+import com.microsoft.onedrivesdk.saver.Saver;
+import com.microsoft.onedrivesdk.saver.SaverException;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import static android.content.ContentValues.TAG;
+
 public class MainActivity extends Activity {
 
+    ISaver mSaver;
+    IPicker mPicker;
+    final String ONEDRIVE_APP_ID = "c4d49a90-0c97-4868-a93d-bc1bcbb9b75b";
     final int PIC_GET_FROM_CAMERA = 1;
     final int PIC_GET_FROM_GALLERY = 2;
     ImageView mIvScan;
@@ -54,10 +72,15 @@ public class MainActivity extends Activity {
                 //TODO
                 //Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 //startActivityForResult(i, PIC_GET_FROM_GALLERY);
-                runOneDrive();
+//                runOneDrive();
+                mSaver = Saver.createSaver(ONEDRIVE_APP_ID);
+                File f = new File (pathScanPicture);
+                mSaver.startSaving((Activity) v.getContext(), "xxxxx.jpg", Uri.fromFile(f));
                 return false;
             }
         });
+
+        isStoragePermissionGranted();
     }
 
     @Override
@@ -77,6 +100,28 @@ public class MainActivity extends Activity {
             cursor.close();
 
             mIvScan.setImageBitmap(resizePicture(pathScanPicture, mIvScan.getMaxWidth(), mIvScan.getMaxHeight()));
+        }
+        if (requestCode == 0xF1F1) {
+            try {
+                mSaver.handleSave(requestCode, resultCode, data);
+            } catch (final SaverException ex) {
+                // Log error information
+                Log.e("OneDriveSaver", "x" + ex.getErrorType().toString()); // Provides one of the SaverError enum
+                Log.d("OneDriveSaver", "x" + ex.getDebugErrorInfo()); // Detailed debug error message
+            }
+        }
+        if (requestCode == 0xF0F0) {
+            // Get the results from the picker
+            IPickerResult result = mPicker.getPickerResult(requestCode, resultCode, data);
+            // Handle the case if nothing was picked
+            if (result != null) {
+                // Do something with the picked file
+                Log.d("main", "Link to file '" + result.getName() + ": " + result.getLink());
+                return;
+            }
+
+            // Handle non-OneDrive picker request
+            super.onActivityResult(requestCode, resultCode, data);
         }
     }
 
@@ -140,5 +185,30 @@ public class MainActivity extends Activity {
     }
 
     void runOneDrive() {
+        Toast.makeText(MainActivity.this, pathScanPicture, Toast.LENGTH_LONG);
+        mPicker = Picker.createPicker(ONEDRIVE_APP_ID);
+        mPicker.startPicking(MainActivity.this, LinkType.WebViewLink);
+        //mSaver = Saver.createSaver(ONEDRIVE_APP_ID);
+        //File f = new File (getFilesDir(), pathScanPicture);
+        //mSaver.startSaving(MainActivity.this, "xxxxx.jpg", Uri.fromFile(f));
+    }
+
+    public  boolean isStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG,"Permission is granted");
+                return true;
+            } else {
+
+                Log.v(TAG,"Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG,"Permission is granted");
+            return true;
+        }
     }
 }
